c224bf8bf5fce750bd5d6750999fd37f2ea4a199
pedrolucasfv <pedro.lucas.tukinha25@gmail.com>
2022-11-30 11:52:11 -0300

    Testes de light/dark mode menu

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -11,4 +11,18 @@ describe('<Menu />', () => {
       screen.getByRole('heading', { name: /where in the world/i })
     ).toBeInTheDocument()
   })
+  it('should render light mode by default', () => {
+    const { container } = renderWithTheme(<Menu />)
+
+    expect(container.firstChild).toHaveStyle({
+      color: 'hsl(209, 23%, 22%)'
+    })
+  })
+  it('should render in dark mode', () => {
+    const { container } = renderWithTheme(<Menu color="dark" />)
+
+    expect(container.firstChild).toHaveStyle({
+      color: 'hsl(0, 0%, 100%)'
+    })
+  })
 })
