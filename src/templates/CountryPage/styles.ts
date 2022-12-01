0c85de08a40f1fc2a3f5ce1eb53eccfc8a5d575b
pedrolucasfv <pedro.lucas.tukinha25@gmail.com>
2022-12-01 02:12:28 -0300

    Deixando CountryPage responsivo

--- src/templates/CountryPage/styles.ts
+++ src/templates/CountryPage/styles.ts
@@ -1,4 +1,5 @@
 import styled, { css, DefaultTheme } from 'styled-components'
+import media from 'styled-media-query'
 import { CountryPageProps } from '.'
 
 type WrapperProps = Pick<CountryPageProps, 'color'>
@@ -14,30 +15,49 @@ const wrapperModifiers = {
 }
 export const Wrapper = styled.main<WrapperProps>`
   ${({ theme, color }) => css`
-    display: grid;
     height: 100%;
+    ${media.lessThan('medium')`
+    display: flex;
+      flex-direction: column;
+      
+    `}
+    ${media.greaterThan('medium')`
+    display: grid;
+    grid-template-rows: 10% 24% 47% 19%;
+    grid-template-columns: 10% 34% 12% 34% 10%;
+    `}
+    ${media.greaterThan('huge')`
     grid-template-rows: 7% 21% 44% 28%;
-    grid-template-columns: 7% 40% 12% 41%;
+    grid-template-columns: 12% 33% 12% 31% 12%;
+    `}
     ${wrapperModifiers[color](theme)}
   `}
 `
 
 export const Menu = styled.div`
-  grid-column: 1/5;
+  grid-column: 1/6;
   grid-row: 1;
 `
 
 export const Info = styled.div`
+  ${media.greaterThan('huge')`
   margin-top: 7rem;
+  `}
   grid-column: 4;
   grid-row: 3;
+  ${media.lessThan('medium')`
+    padding-left: 2.5rem
+  `}
 `
 type FlagProps = {
   src: string
 }
 export const Image = styled.div<FlagProps>`
   ${({ src }) => css`
-    height: 100%rem;
+    ${media.lessThan('medium')`
+  margin: 3.5rem 3rem;
+  `}
+    height: 100%;
     background-image: url(${src});
     background-position: center;
     background-size: cover;
@@ -47,6 +67,9 @@ export const Image = styled.div<FlagProps>`
 `
 
 export const Button = styled.div`
+  ${media.lessThan('medium')`
+  padding: 3rem 0rem 0rem 3rem;
+  `}
   grid-column: 2;
   grid-row: 2;
   display: flex;
